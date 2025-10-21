dc0b5e5e45551ad585f4216a6b1f959c75938b34
kimwaki <moreenkim36@gmail.com>
2025-10-21 08:15:21 +0300

    create and update the footer with links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import { Box, Text, Link, HStack } from '@chakra-ui/react';
 
 const Footer = () => {
   return (
-    <Box bg="gray.800" color="white" py={6} px={4} textAlign="center">
+    <Box bg="gray.800" color="white" py={4} px={4} textAlign="center">
       <HStack spacing={4} justify="center" mb={2}>
         <Link href="https://www.linkedin.com/in/moreen-kimwaki-a74b49126/" isExternal>LinkedIn</Link>
         <Link href="https://github.com/moreen" isExternal>GitHub</Link>
